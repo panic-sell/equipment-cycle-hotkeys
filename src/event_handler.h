2ba4aca89d3ba06cba6141aea9ac10ce1de2f0ac
panic-sell <150742160+panic-sell@users.noreply.github.com>
2023-12-30 10:09:00 +0100

    remove feature that deselects hotkeys on equip events

--- src/event_handler.h
+++ src/event_handler.h
@@ -65,41 +65,32 @@ InspectEquipped(std::span<const Keystroke> keystrokes, RE::Actor& actor) {
 
 }  // namespace internal
 
-class EventHandler final : public RE::BSTEventSink<RE::InputEvent*>,
-                           public RE::BSTEventSink<RE::TESEquipEvent> {
+class EventHandler final : public RE::BSTEventSink<RE::InputEvent*> {
   public:
     [[nodiscard]] static std::expected<void, std::string_view>
     Init(Hotkeys<>& hotkeys, std::mutex& hotkeys_mutex) {
         auto* idm = RE::BSInputDeviceManager::GetSingleton();
-        auto* sesh = RE::ScriptEventSourceHolder::GetSingleton();
-        if (!idm || !sesh) {
-            return std::unexpected("cannot get event sources");
+        if (!idm) {
+            return std::unexpected("cannot get input event source");
         }
 
-        static EventHandler instance;
-        instance.hotkeys_ = &hotkeys;
-        instance.hotkeys_mutex_ = &hotkeys_mutex;
+        static auto instance = EventHandler(hotkeys, hotkeys_mutex);
         idm->AddEventSink<RE::InputEvent*>(&instance);
-        sesh->AddEventSink<RE::TESEquipEvent>(&instance);
         return {};
     }
 
-    /// Triggers hotkey activations.
     RE::BSEventNotifyControl
     ProcessEvent(RE::InputEvent* const* events, RE::BSTEventSource<RE::InputEvent*>*) override {
         HandleInputEvents(events);
         return RE::BSEventNotifyControl::kContinue;
     }
 
-    /// Deselects hotkeys if something else equips/unequips player gear.
-    RE::BSEventNotifyControl
-    ProcessEvent(const RE::TESEquipEvent* event, RE::BSTEventSource<RE::TESEquipEvent>*) override {
-        HandleEquipEvent(event);
-        return RE::BSEventNotifyControl::kContinue;
-    }
-
   private:
-    EventHandler() = default;
+    EventHandler(Hotkeys<>& hotkeys, std::mutex& hotkeys_mutex)
+        : RE::BSTEventSink<RE::InputEvent*>(),
+          hotkeys_(&hotkeys),
+          hotkeys_mutex_(&hotkeys_mutex) {}
+
     EventHandler(const EventHandler&) = delete;
     EventHandler& operator=(const EventHandler&) = delete;
     EventHandler(EventHandler&&) = delete;
@@ -145,11 +136,6 @@ class EventHandler final : public RE::BSTEventSink<RE::InputEvent*>,
         if (!current || orig == current) {
             return;
         }
-
-        // Most-recent-equip-time must be reset prior to equipset-apply because the latter
-        // triggers equip events.
-        auto now = RE::GetDurationOfApplicationRunTime();
-        most_recent_hotkey_equip_time_.store(now);
         current->Apply(*aem, *player);
         SKSE::log::debug(
             "selected hotkey {} ({}) equipset {}",
@@ -159,50 +145,11 @@ class EventHandler final : public RE::BSTEventSink<RE::InputEvent*>,
         );
     }
 
-    void
-    HandleEquipEvent(const RE::TESEquipEvent* event) {
-        if (!event || !event->actor || !event->actor->IsPlayerRef()) {
-            return;
-        }
-        const auto* form = RE::TESForm::LookupByID(event->baseObject);
-        if (!form) {
-            return;
-        }
-
-        // Ignore equip/unequip actions on items that don't map to supported gear slots.
-        switch (form->GetFormType()) {
-            case RE::FormType::Armor:
-                if (!tes_util::IsShield(form)) {
-                    return;
-                }
-            case RE::FormType::Spell:
-            case RE::FormType::Weapon:
-            case RE::FormType::Light:
-            case RE::FormType::Ammo:
-            case RE::FormType::Shout:
-                break;
-            default:
-                return;
-        }
-
-        // Assume any equip event within 0.5 s of a hotkey activation is the result of that hotkey
-        // activation.
-        auto now = RE::GetDurationOfApplicationRunTime();
-        if (now <= most_recent_hotkey_equip_time_.load() + 500) {
-            return;
-        }
-        auto lock = std::lock_guard(*hotkeys_mutex_);
-        hotkeys_->Deselect();
-        SKSE::log::debug("deselected all hotkeys");
-    }
-
-    Hotkeys<>* hotkeys_ = nullptr;
-    std::mutex* hotkeys_mutex_ = nullptr;
+    Hotkeys<>* hotkeys_;
+    std::mutex* hotkeys_mutex_;
     /// Reusable buffer for storing input keystrokes and avoiding per-input-event allocations. We
     /// assume `HandleInputEvents()` will only be called from one thread at a time.
     std::vector<Keystroke> keystroke_buf_;
-    /// In milliseconds since application start.
-    std::atomic<uint32_t> most_recent_hotkey_equip_time_ = 0;
 };
 
 }  // namespace ech
